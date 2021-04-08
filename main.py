965c374dd6e156b55eeb79b243932fe6eabb6960
Richard's MacBook Pro <rb1113d@gmail.com>
2021-04-08 23:15:19 +0800

    0.05 Added real time display
    Added real time display of ultrasonic sensor reading and current time to GUI, added keyboard hotkey for beep, tweaked GUI elements

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # import Libraries
-from flask import Flask, render_template, Response, request
+from flask import Flask, render_template, Response, request, jsonify
 from camera import VideoCamera
 import time
 import threading
@@ -8,38 +8,46 @@ from time import sleep
 
 import ultrasonic
 import motor
+import datetime
 
 pi_camera = VideoCamera(flip=True)  # flip pi camera if upside down.
 
 # App Globals (do not edit)
 app = Flask(__name__)
 
+running = True  # to control loop in thread
+value = 0
+
 # thread for ultrasonic.distance
+
+
 @app.before_first_request
 def activate_job():
     def run_job():
-            while True:
-                sleep(0.5)
-                #print("Run recurring task")
-                uDist = ultrasonic.distance()
-                ultrasonic.alarm(uDist)
-                #print("%.1f" % uDist)
-                templateData = {
-                    'dist' : uDist
-                }
-                #return render_template('index.html', **templateData)
+        global value
+        while True:
+            sleep(0.5)
+            uDist = ultrasonic.distance()
+            ultrasonic.alarm(uDist)
+            value = "Distance: " + "{:.2f}".format(uDist)
+            #print("%.1f" % uDist)
 
     threadU = threading.Thread(target=run_job)
     threadU.start()
 
 # routing
 
+
 # root
+
+
 @app.route('/')
 def index():
     return render_template('index.html')  # you can customze index.html here
 
 # action
+
+
 @app.route("/<action>")
 def act(action):
     if action == "beep":
@@ -47,24 +55,36 @@ def act(action):
         sleep(1)
         ultrasonic.beep(0)
     elif action == "forward":
-            motor.forward()
-            print("forward")
+        motor.forward()
+        print("forward")
     elif action == "backward":
-            motor.backward()
-            print("backward")
+        motor.backward()
+        print("backward")
     elif action == "left":
-            motor.left()
-            print("left")
+        motor.left()
+        print("left")
     elif action == "right":
-            motor.right()
-            print("right")
+        motor.right()
+        print("right")
     elif action == "stop":
-            motor.stop()
-            print("stop")
+        motor.stop()
+        print("stop")
     else:
         print("")
 
     return render_template('index.html')
+
+# update
+
+
+@app.route('/update', methods=['POST'])
+def update():
+    return jsonify({
+        'value': value,
+        'time': datetime.datetime.now().strftime("%H:%M:%S"),
+    })
+
+# handler for camera
 
 
 def gen(camera):
@@ -75,11 +95,14 @@ def gen(camera):
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 # video steram
+
+
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(pi_camera), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
+# run main
 if __name__ == '__main__':
     try:
         app.run(host='0.0.0.0', port='8000', debug=False)
